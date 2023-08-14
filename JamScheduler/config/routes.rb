6f29c977d611f9e41b602059783eac0aed966201
nj <nickgentrymma@gmail.com>
2023-08-14 01:06:35 -0700

    added events routes

--- JamScheduler/config/routes.rb
+++ JamScheduler/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:show, :create, :update]
     resources :songs, only: [:index, :show, :create, :update, :destroy]
+    resources :events, only: [:index, :show, :create, :update, :destroy]
     resource :session, only: [:show, :create, :destroy]
   end
 end
