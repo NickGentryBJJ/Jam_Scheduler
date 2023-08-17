e2468ea27fc0ba8c0765a00d70f76e06588f739b
nj <nickgentrymma@gmail.com>
2023-08-17 02:19:29 -0700

    sign up page fully styled

--- JamScheduler/frontend/src/components/SignUp/SignUpForm.jsx
+++ JamScheduler/frontend/src/components/SignUp/SignUpForm.jsx
@@ -66,61 +66,64 @@ function SignupForm() {
   <>
     <div className="sign-up-wrapper">
       <h2 className="make">Are You Ready To Jam? Join Now!</h2>
-        <div className="sign-up-form">
-          <form onSubmit={handleSubmit}>    
-          <ul>
-            {errors.map(error => <li className="errors" key={error}>{error}</li>)}
-          </ul>
-          <label>
-            Email<br/>
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          </label>
-          <br/>
-          <label>
-            Stage Name<br/>
-          <input
-            type="text"
-            value={stageName}
-            onChange={(e) => setStageName(e.target.value)}
-            required
-          />
-          </label>
-          <br/>
-          <label>
-            Password<br/>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-          </label>
-          <br/>
-          <label>
-            Confirm Password<br />
-          <input
-            type="password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
+        <div className="middle-sign-up">
+          <div className="sign-up-form">
+            <form onSubmit={handleSubmit}>    
+            <ul>
+              {errors.map(error => <li className="errors" key={error}>{error}</li>)}
+            </ul>
+            <label>
+              Email<br/>
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
             />
-          </label>
-          <br/>
-          <input type="file" onChange={handleFile} />
-          <h3>Image preview</h3>
-          {preview}
-          <br/>
-            <div className="agree">
-              <p>By clicking Agree & Join, you agree to the Jam Club User Agreement, Privacy Policy, and Cookie Policy.</p>
-            </div>
-          <button type="submit" className="agree-button">Agree & Join</button>
-          </form>
-        </div>
+            </label>
+            <br/>
+            <label>
+              Stage Name<br/>
+            <input
+              type="text"
+              value={stageName}
+              onChange={(e) => setStageName(e.target.value)}
+              required
+            />
+            </label>
+            <br/>
+            <label>
+              Password<br/>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            </label>
+            <br/>
+            <label>
+              Confirm Password<br />
+            <input
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+              />
+            </label>
+            <br/>
+            <input type="file" onChange={handleFile} />
+            <h3>Image preview</h3>
+            {preview}
+            <br/>
+              <div className="agree">
+                <p>By clicking Agree & Join, you agree to the Jam Club User Agreement, Privacy Policy, and Cookie Policy.</p>
+              </div>
+            <button type="submit" className="agree-button">Agree & Join</button>
+            </form>
+          </div>
+          <h3 className="jam-message">Get jamming with your friends on Jam Club!</h3>
+      </div>
     </div>
   </>
   );
