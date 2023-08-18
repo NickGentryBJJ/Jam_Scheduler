f0b86633cdbb91c09d2ed09fdf222c40380d74a2
nj <nickgentrymma@gmail.com>
2023-08-18 04:42:33 -0700

    added

--- JamScheduler/frontend/src/components/Events/EventShow/EventShow.jsx
+++ JamScheduler/frontend/src/components/Events/EventShow/EventShow.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import './EventShow.css'
-import { fetchEvent, getEvent } from "../../../store/events";
+import { fetchEvent, fetchEvents, getEvent } from "../../../store/events";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import EventShowItem from "../EventShowItem/EventShowItem";
 
@@ -15,9 +15,13 @@ function EventShow() {
     const events = useSelector(state => state.events);
     const event = events[eventId]
     return (
-        <div className="event-show-wrapper">
-            <EventShowItem event={event}/>
-        </div>
+        <>
+        {event ? 
+            <div className="event-show-wrapper">
+                <EventShowItem event={event}/>
+            </div>
+        : null}
+        </>
     )
 }
 
--- JamScheduler/frontend/src/components/Events/EventShowItem/EventShowItem.jsx
+++ JamScheduler/frontend/src/components/Events/EventShowItem/EventShowItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import UserEventInfo from "../UserEventInfo/UserEventInfo";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import './EventShowItem.css'
 
 function EventShowItem({event}) {
     const history = useHistory();
@@ -11,24 +12,28 @@ function EventShowItem({event}) {
 
 
     return (
-        <div className="event-index-item-wrapper">
-        <div className="top-of-event">
-            <UserEventInfo event={event}/> 
-            <h1 className="event-title">{event.title}</h1>
-        </div>
-    <div className="event-index-item-container">
-        <div className="event-info-container">
-            <h2 className="event-location">Event Location: {event.location}</h2>
-            <p className="event-details">{event.details}</p>
-            <span className="event-date">{event.date}</span><br/>
-            <div className="event-buttons">
-                <button onClick={backToIndex} className="back-to-index-button">Back To All Events</button>
-                <button className="show-participants">Show Participants</button>
+        <>
+        {event ? 
+            <div className="event-show-item-wrapper">
+                <div className="top-of-event">
+                    <UserEventInfo event={event}/> 
+                    <h1 className="event-title">{event.title}</h1>
+                </div>
+                <div className="event-show-item-container">
+                    <div className="event-info-container">
+                        <h2 className="event-location">Event Location: {event.location}</h2>
+                        <p className="event-details">{event.details}</p>
+                        <span className="event-date">{event.date}</span><br/>
+                        <div className="event-buttons">
+                            <button onClick={backToIndex} className="back-to-index-button">Back To All Events</button>
+                            <button className="show-guests">Show Guests</button>
+                            <button className="event-comment-button">Add Comment</button>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-</div>
-
+        : null}
+        </>
     )
 }
 
