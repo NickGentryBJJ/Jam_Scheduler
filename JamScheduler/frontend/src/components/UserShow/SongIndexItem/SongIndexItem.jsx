e65d1fc1520a043590a0e6d70461780fba1ed0da
nj <nickgentrymma@gmail.com>
2023-08-19 21:08:03 -0700

    Songs have Full CRUD. Edit functionality complete

--- JamScheduler/frontend/src/components/UserShow/SongIndexItem/SongIndexItem.jsx
+++ JamScheduler/frontend/src/components/UserShow/SongIndexItem/SongIndexItem.jsx
@@ -1,17 +1,37 @@
 import React, { useState } from "react";
 import './SongIndexItem.css'
 import { useDispatch, useSelector } from "react-redux";
-import { deleteSong } from "../../../store/songs";
+import { deleteSong, updateSong } from "../../../store/songs";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 
 function SongIndexItem(song) {
+
     const dispatch = useDispatch();
-    const sessionUser = useSelector(state => state.session.user)
     const history = useHistory();
+
+    const sessionUser = useSelector(state => state.session.user);
+    const [editing, setEditing] = useState(false);
+    const [songName, setSongName] = useState(song.song.songName);
+    const [ogArtist, setOgArtist] = useState(song.song.originalArtist);
+    const [desiredInstrument, setDesiredInstrument] = useState(song.song.desiredInstrument);
+
+
     const handleDelete = () => {
         history.go();
         dispatch(deleteSong(song.song.id));
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('song[songName]', songName);
+        formData.append('song[originalArtist]', ogArtist);
+        formData.append('song[desiredInstrument]', desiredInstrument);
+        formData.append('song[userId]', sessionUser.id );
+        dispatch(updateSong(song.song, formData))
+        setEditing(false)
+        history.go();
     }
 
 
@@ -19,7 +39,46 @@ function SongIndexItem(song) {
     // on the user associated with the song. pretty neat!
     
     let remix;
-    if (song.song.userId === sessionUser.id) {
+    if (song.song.userId === sessionUser.id && editing === true) {
+        remix = (
+            <div className="song-index-item">
+                <div className="wrapper-song">
+                    <div className="song-info">
+                        <form onSubmit={handleSubmit}>
+                            <div 
+                            className="song-title"
+                            contentEditable
+                            onInput={(e) => {
+                                setSongName(e.target.innerText)
+                            }}
+                            dangerouslySetInnerHTML={{ __html: song.song.songName }}
+                            />
+                            <div 
+                            className="song-artist"
+                            contentEditable
+                            onInput={(e) => {
+                                setOgArtist(e.target.innerText)
+                            }}
+                            dangerouslySetInnerHTML={{ __html: song.song.originalArtist }}
+                            />
+                            <div
+                            className="desired-instrument"
+                            contentEditable
+                            onInput={(e) => {
+                                setDesiredInstrument(e.target.innerText)
+                            }}
+                            dangerouslySetInnerHTML={{ __html: song.song.desiredInstrument }}
+                            />
+                            <div className="mod-menue">
+                                <button type="submit" className="edit-song-button">Save Song</button>
+                                <button onClick={()=>{setEditing(false)}} className="delete-song-button">Cancel</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        )
+    } else if (song.song.userId === sessionUser.id && editing !== true) {
         remix = (
         <div className="song-index-item">
             <div className="wrapper-song">
@@ -39,13 +98,13 @@ function SongIndexItem(song) {
                     </h2>
                 </div>
                 <div className="mod-menue">
-                    <button className="edit-song-button">Edit</button>
+                    <button onClick={()=>{setEditing(true)}} className="edit-song-button">Edit</button>
                     <button onClick={handleDelete} className="delete-song-button">Delete</button>
                 </div>
             </div>
         </div>
         )
-    } else {
+    } else if(song.song.userId !== sessionUser.id) {
         remix = (
         <div className="song-index-item">
             <h1 className="song-title">{song.song.songName}</h1>
