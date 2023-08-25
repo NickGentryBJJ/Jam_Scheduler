c37c00b706578245c6f6238ed828619683f5d526
nj <nickgentrymma@gmail.com>
2023-08-24 21:11:07 -0700

    added buttons to edit and delete status attendee stuffs

--- JamScheduler/frontend/src/components/Events/Attendees/AttendeeListItem/AttendeeListItem.jsx
+++ JamScheduler/frontend/src/components/Events/Attendees/AttendeeListItem/AttendeeListItem.jsx
@@ -1,50 +1,59 @@
 import React from "react";
 import './AttendeeListItem.css'
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { useSelector } from "react-redux";
 
 const AttendeeListItem = ({attendee, user, index}) => {
+    const sessionUser = useSelector(state=> state.session.user)
     const history = useHistory();
-    
     const handleUser = () => {
         history.push(`/users/${user.id}`)
     }
-
-    // if (attendee.status === "Going"){
-    //     const gButton = document.querySelector('.attendee-list-item-wrapper')
-    //     gButton.style.background = 'limegreen';
-    // } else {
-    //     const gButton = document.querySelector('.attendee-list-item-wrapper')
-    //     gButton.style.background = '#cbcb00';
-    // }
     let korra;
-    if (attendee.status === "Going"){
+    if (attendee.status === "Going" && user.id !== sessionUser.id){
         korra = (
             <div style={{ background: 'limegreen' }} className="attendee-list-item-wrapper">
-                <ul key={index}>
+                <ul className="attendee-list-cont" key={index}>
                     <li className="user-att-name" onClick={handleUser}>{user.stageName}</li>
                     <li>Status: {attendee.status}</li>
                 </ul>
             </div>
         )
-    } else {
+    } else if (attendee.status === "Maybe" && user.id !== sessionUser.id) {
         korra =(
-            <div style={{ background: 'yellow' }} className="attendee-list-item-wrapper">
-                <ul key={index}>
+            <div style={{ background: '#c7c701' }} className="attendee-list-item-wrapper">
+                <ul className="attendee-list-cont" key={index}>
                     <li className="user-att-name" onClick={handleUser}>{user.stageName}</li>
                     <li>Status: {attendee.status}</li>
+                </ul>
+            </div>
+        )
+    } else if (attendee.status === "Going" && user.id === sessionUser.id){
+        korra = (
+            <div style={{ background: 'limegreen' }} className="attendee-list-item-wrapper">
+                <ul className="attendee-list-cont" key={index}>
+                    <li className="user-att-name" onClick={handleUser}>{user.stageName}</li>
+                    <li>Status: {attendee.status}</li>
+                </ul>
+            </div>
+        )
+    } else if (attendee.status === "Maybe" && user.id === sessionUser.id) {
+        korra =(
+            <div style={{ background: '#c7c701' }} className="attendee-list-item-wrapper">
+                <ul className="attendee-list-cont" key={index}>
+                    <li className="user-att-name" onClick={handleUser}>{user.stageName}</li>
+                    <li>Status: {attendee.status}</li>
+                    <div className="mod-buttons">
+                    <button className="delete-status-button">Delete</button>
+                    <button className="edit-status-button">Edit</button>
+                    </div>
                 </ul>
             </div>
         )
     }
-    // <div style={{ background: 'yellow' }} className="attendee-list-item-wrapper">
-    //     <ul key={index}>
-    //         <li className="user-att-name" onClick={handleUser}>{user.stageName}</li>
-    //         <li>Status: {attendee.status}</li>
-    //     </ul>
-    // </div>
     return (
         <>
-        {korra}
+            {korra}
         </>
     )
 }
